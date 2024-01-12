2af0f95b4ed885c0a252b7951fdc0661ac145d37
Karn <140336765+kxxnD04@users.noreply.github.com>
2024-01-12 10:38:50 +0700

    add file

--- week5/lab05/src/Plane.java
+++ week5/lab05/src/Plane.java
@@ -3,13 +3,14 @@ public class Plane extends Vehicle{
         setFuel(s);
         setTopSpeed(t);
     }
-    public void fly(){
-        System.out.println(getFuel() >= 200 ? "Fly.":"Please add fuel.");
-        if (getFuel() >= 200){
+    public void fly() {
+        System.out.println(getFuel() >= 200 ? "Fly." : "Please add fuel.");
+        if (getFuel() >= 200) {
             setFuel(getFuel() - 200);
         }
     }
-    public void showPlaneInfo(){
-        System.out.println("Plane detail is, " + "Fuel is " + getFuel()+ " litre and Top Speed is "+ getTopSpeed() + " m/s.");
+
+    public void showPlaneInfo() {
+        System.out.println("Plane detail is, " + "Fuel is " + getFuel() + " litre and Top Speed is " + getTopSpeed() + " m/s.");
     }
 }
---
+++ week6/lab06/src/Employee.java
@@ -0,0 +1,49 @@
+public class Employee {
+    private static String nationality = "Thai";
+    private String name;
+    private Wallet wallet;
+    private int energy;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public Wallet getWallet(){
+        return wallet;
+    }
+    public void setWallet(Wallet wallet){
+        this.wallet = wallet;
+    }
+    public int getEnergy(){
+        return energy;
+    }
+    public void setEnergy(int energy){
+        this.energy = energy;
+    }
+    public static String getNationality(){
+        return nationality;
+    }
+    public static void setNationality(String nationality){
+        Employee.nationality = nationality;
+    }
+    public void eat(Food f){
+        energy += f.getEnergy();
+    }
+    public boolean buyFood(Seller s){
+        if (s != null){
+            this.eat(s.sell(this));
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean equals(Employee e){
+        return e.getName().equals(this.name);
+    }
+    @Override
+    public String toString(){
+        return ("My name is " + this.name + "." + "\nI have " + this.energy 
+                + " energy left.\nI have a balance of " + this.wallet.getBalance() + " baht.");
+    }
+}
